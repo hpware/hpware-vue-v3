353af9b4ed8fcea6542894e1ddb044cfdb6d89f2
Howard <hw@yuanhau.com>
2024-11-18 12:55:20 +0800

    Remove unused test component and add new loading components with animations

---
+++ src/Pages/Beta/Loading.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { animate, scroll } from 'motion';
+const loading = ref(null);
+
+onMounted(() => {
+  animate(
+    loading.value,
+    { rotate: 90 },
+    { type: "spring", ease: "easeInOut",repeat: Infinity, repeatDelay: 0.2 }
+  );
+});
+</script>
+<template>
+  <div class="center">
+    <!--Loading Animation-->
+    <div class="loading" ref="loading"></div>
+</div>
+</template>
+<style scoped>
+.loading {
+  width: 100px;
+  height: 100px;
+  border-radius: 10px;
+  background-color: white;
+}
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
