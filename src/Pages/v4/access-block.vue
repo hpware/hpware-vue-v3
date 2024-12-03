ba883e5945bdc171f68365c5c036246cf132650c
Howard <hw@hwtw.cc>
2024-12-03 22:57:23 +0800

    Update Projects.vue repository link and add access-block.vue for permission handling

---
+++ src/Pages/v4/access-block.vue
@@ -0,0 +1,8 @@
+<template>
+    <title>v4 Access Block | 吳元皓</title>
+    <div>
+        <h2>你沒有權限使用 yuanhau-v4!</h2>
+        <h3>如果這是一個錯誤 <a href="https://v4.yuanhau.com/">請重試</a></h3>
+        <p>如果想要試用並提出建議(在 2025 Q2 前) 請 Email: <a href="mailto:hw+try@yuanhau.com">hw@yuanhau.com</a>!</p>
+    </div>
+</template>
