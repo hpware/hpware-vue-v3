995194bef885f391c3ca2cf6ea21f7af05ebb21d
yuanh <157942818+hpware@users.noreply.github.com>
2024-12-20 17:19:10 +0800

    Update redirect_here.vue

--- src/Pages/redirect_here.vue
+++ src/Pages/redirect_here.vue
@@ -1,3 +1,9 @@
+<script setup>
+  const goLink = new URL(window.location.href).SearchParams.get("go");
+  setTimeOut(() => {
+      window.location.href = goLink;
+  }, 3000)
+</script>
 <template>
   <div>
     <h2>台灣是一個國家! 不要讓國民黨摧毀一切!</h2>
